34d5338f2bbd6e935f802331eed2d653ee923e7a
kishalov <122756429+kishalov@users.noreply.github.com>
2026-05-30 13:17:17 +0000

    og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
 		type: "website",
 		images: [
 			{
-				url: "/images/og-image.jpg",
+				url: "/images/og-image.png",
 				width: 1200,
 				height: 630,
 				alt: "Портфолио Алины Камаловой",
